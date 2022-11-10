851828c77729f3b4df9b89858e1d878fcdcdf553
Hector <hectoreduardo354@hotmail.com>
2022-11-09 18:53:24 -0600

    Se implemento la funcion de getMovies que corresponde al Read del CRUD

--- app/src/main/java/com/example/practica2/db/DbMovies.kt
+++ app/src/main/java/com/example/practica2/db/DbMovies.kt
@@ -2,6 +2,8 @@ package com.example.practica2.db
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
+import com.example.practica2.model.Movie
 
 class DbMovies(private val context:Context):DbHelper(context) {
     //Esta clase implementa las opreaciones CRUD
@@ -26,5 +28,25 @@ class DbMovies(private val context:Context):DbHelper(context) {
         }
 
         return id
+    }
+    fun getMovies():ArrayList<Movie>{
+        val dbHelper = DbHelper(context)
+        val db = dbHelper.writableDatabase
+
+        var listMovies = ArrayList<Movie>()
+        var movieTmp:Movie? = null
+        var cursorMovies:Cursor? = null
+
+        cursorMovies = db.rawQuery("SELECT * FROM MOVIES",null)
+        if(cursorMovies.moveToFirst()){
+            do {
+                movieTmp = Movie(cursorMovies.getInt(0),cursorMovies.getString(1),cursorMovies.getString(2),cursorMovies.getInt(3),cursorMovies.getInt(4))
+                listMovies.add(movieTmp)
+            }while(cursorMovies.moveToNext())
+        }
+
+        cursorMovies.close()
+
+        return listMovies
     }
 }
--- app/src/main/java/com/example/practica2/view/activities/InsertActivity.kt
+++ app/src/main/java/com/example/practica2/view/activities/InsertActivity.kt
@@ -1,11 +1,13 @@
 package com.example.practica2.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.practica2.R
 import com.example.practica2.databinding.ActivityInsertBinding
-import com.example.practica2.databinding.ActivityMainBinding
+import com.example.practica2.db.DbMovies
 
 class InsertActivity : AppCompatActivity() {
     private lateinit var binding:ActivityInsertBinding
@@ -18,6 +20,44 @@ class InsertActivity : AppCompatActivity() {
     }
 
     fun click(view: View) {
+        val dbMovies = DbMovies(this)
+        with(binding){
+            when{
+                tietTitulo.text.toString().isEmpty() -> {
+                    tietTitulo.error = "No puede quedar vacío"
+                    Toast.makeText(this@InsertActivity, "Por favor llene todos los campos", Toast.LENGTH_SHORT).show()
+                }
+                tietGenero.text.toString().isEmpty() -> {
+                    tietGenero.error = "No puede quedar vacío"
+                    Toast.makeText(this@InsertActivity, "Por favor llene todos los campos", Toast.LENGTH_SHORT).show()
+                }
+                tietAnio.text.toString().isEmpty() -> {
+                    tietAnio.error = "No puede quedar vacío"
+                    Toast.makeText(this@InsertActivity, "Por favor llene todos los campos", Toast.LENGTH_SHORT).show()
+                }
+                tietValoracion.text.toString().isEmpty() -> {
+                    tietAnio.error = "No puede quedar vacío"
+                    Toast.makeText(this@InsertActivity, "Por favor llene todos los campos", Toast.LENGTH_SHORT).show()
+                }else -> {
+                    //Realizamos la inserción
+                    val id = dbMovies.insertMovie(tietTitulo.text.toString(), tietGenero.text.toString(),tietAnio.text.toString().toInt(),tietValoracion.text.toString().toInt())
+                    if(id>0){
+                        Toast.makeText(this@InsertActivity, "Registro guardado exitosamente", Toast.LENGTH_SHORT).show()
+                        tietTitulo.setText("")
+                        tietGenero.setText("")
+                        tietAnio.setText("")
+                        tietValoracion.setText("")
+                        tietTitulo.requestFocus()
+                    }else{
+                        Toast.makeText(this@InsertActivity, "Error al guardar el registro", Toast.LENGTH_SHORT).show()
+                    }
 
+                }
+            }
+        }
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
